d1d549151f0437bfe8b676bfe4d583790f85054d
Ovieh Mosley <ovieh@intellisense.io>
2020-06-20 13:58:29 +0100

    made a bunch of arbitrary changes to test deployment workflow

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -5,7 +5,8 @@ import { OrderCancelledListener } from './events/listeners/order-cancelled-liste
 import { OrderCreatedListener } from './events/listeners/order-created-listener';
 
 const start = async () => {
-  console.log('Starting....');
+  console.log('starting up 🚀');
+
   if (!process.env.JWT_KEY) {
     throw new Error('JWT must be defined!');
   }
