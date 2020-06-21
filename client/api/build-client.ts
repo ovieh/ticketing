68a35a8f22268fa385ecfc9d8d7ae6f2c7096933
Ovieh Mosley <ovieh@intellisense.io>
2020-06-21 17:35:36 +0100

    client now works in development

--- client/api/build-client.ts
+++ client/api/build-client.ts
@@ -4,7 +4,10 @@ export default async function buildClient({ req }) {
 	if (typeof window === 'undefined') {
 		// we are in the server
 		return axios.create({
-			baseURL: 'http://www.ticketing-prod.xyz',
+			baseURL:
+				process.env.NODE_ENV === 'development'
+					? 'http://ingress-nginx-controller.kube-system.svc.cluster.local'
+					: 'http://www.ticketing-prod.xyz',
 			headers: req.headers,
 		});
 	} else {
