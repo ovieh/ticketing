cdc0847d35c9ec381099f216df7e7a8cd7e4f912
Ovieh Mosley <ovieh@intellisense.io>
2020-06-22 23:50:00 +0100

    changed dockerfile image, created card component

---
+++ client/components/Card.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Ticket } from '../types';
+
+interface CardProps {
+	ticket: Ticket;
+}
+
+export const Card: React.FC<CardProps> = ({ ticket: { price, title }, children }) => {
+	return (
+		<div className="overflow-hidden bg-dark-blue max-w-sm rounded shadow-lg mb-2 mt-2">
+			<img className="w-full" src={require("../public/img/card-image.jpeg?size=336")} alt="Concert Image" />
+			<div className="px-6 py-4">
+				<h3 className="text-2xl font-bold mb-2">{title}</h3>
+				<p className="text-lg mb-2">${price.toFixed(2)}</p>
+                {children}
+			</div>
+		</div>
+	);
+};
