816aa1d235cb28a1165a096a98914da5d9c21fe9
Ovieh Mosley <ovieh@intellisense.io>
2020-06-23 09:32:28 +0100

    used cards for orders

--- client/components/Card.tsx
+++ client/components/Card.tsx
@@ -8,11 +8,17 @@ interface CardProps {
 export const Card: React.FC<CardProps> = ({ ticket: { price, title }, children }) => {
 	return (
 		<div className="overflow-hidden bg-dark-blue max-w-sm rounded shadow-lg mb-2 mt-2">
-			<img className="w-full" src={require("../public/img/card-image.jpeg?size=336")} alt="Concert Image" />
+			<img
+				className="w-full"
+				src={require('../public/img/card-image.jpeg?size=336')}
+				alt="Concert Image"
+			/>
 			<div className="px-6 py-4">
 				<h3 className="text-2xl font-bold mb-2">{title}</h3>
-				<p className="text-lg mb-2">${price.toFixed(2)}</p>
-                {children}
+				<p className="text-lg mb-2">
+					<span>price: </span>${price.toFixed(2)}
+				</p>
+				{children}
 			</div>
 		</div>
 	);
