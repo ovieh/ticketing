effd649d1325af3f7f2735ea5556a96cf043a330
Ovieh Mosley <ovieh@intellisense.io>
2020-06-26 08:49:05 +0100

    removed extra stripe file

--- payments/stripe.ts
+++
@@ -1,6 +0,0 @@
-import Stripe from 'stripe';
-
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-export const stripe = new Stripe(process.env.STRIPE_KEY!, {
-  apiVersion: '2020-03-02',
-});
