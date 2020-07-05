4dd37704a4cf22ccf327d49b9c9b5de53e3f7139
Ovieh Mosley <ovieh@intellisense.io>
2020-07-05 11:34:59 +0100

    updated message

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -16,7 +16,7 @@ const start = async () => {
   }
 
   if (!process.env.MONGO_URI) {
-    throw new Error('MONGO_URI must be defined!');
+    throw new Error('MONGO_URI must be defined!!');
   }
 
   if (!process.env.NATS_CLUSTER_ID) {
